14fe21087b951c28136cf6265aa9f553cb56b0c1
kamil <kkarpiak94@gmail.com>
2024-03-05 08:45:38 +0100

    Initial Commit

---
+++ KKHelper/KKHelper.h
@@ -0,0 +1,18 @@
+//
+//  KKHelper.h
+//  KKHelper
+//
+//  Created by Kamil Karpiak on 05/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KKHelper.
+FOUNDATION_EXPORT double KKHelperVersionNumber;
+
+//! Project version string for KKHelper.
+FOUNDATION_EXPORT const unsigned char KKHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKHelper/PublicHeader.h>
+
+
